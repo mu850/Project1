42116a237406da03395aaab5fd14dbc188bcb735
mu850 <mu7474674@gmail.com>
2025-09-19 06:41:37 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'flour-mill-calc-v11';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Noto+Naskh+Arabic+Urdu:wght@400..700&display=swap',
+  'https://i.imgur.com/8Qh1eUe.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
